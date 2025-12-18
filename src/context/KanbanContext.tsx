afef63a34315dc13a2a0ef7556e127d3254628ae
SauceCode01 <sauce.code.01@gmail.com>
2025-12-18 15:33:39 +0800

    clean: clean dir

--- src/context/KanbanContext.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React, { createContext, useContext, useMemo, useState } from "react";
-import type { List, Ticket } from "@/components/widgets/Kanban/types";
-
-export interface KanbanContextType {
-  // moved to @/Providers/KanbanProvider
-  tickets: Ticket[];
